7078e33cef4febeaf8ab4746d0fee33804cc48db
alejandro0 <alejandro0giraldo@hotmail.com>
2023-07-18 14:54:47 -0500

    first commit

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,70 @@
+import User from '../models/user.model.js'
+  import bcrypt from 'bcryptjs'
+import { createAccessToken } from '../libs/jwt.js'
+
+export const register= async (req, res)=> {
+    const {email, password, username}=req.body
+try {
+ const passwordHash= await bcrypt.hash(password, 10)
+    const newUser= new User({
+        username,
+        email,
+        password:passwordHash
+     });
+
+const userSaved= await newUser.save();
+const token= await createAccessToken({ id:userSaved._id});
+
+res.cookie('token',token);
+  res.json({
+     id:userSaved._id,
+     username:userSaved.username,
+     email:userSaved.email,
+     createdAt:userSaved.createdAt,
+     updatedAt:userSaved.updatedAt
+   }); 
+} catch (error) {
+  res.status(500).json({ menssage: error.menssage});
+}
+};
+export const login= async (req, res)=> {
+  const {email, password}=req.body
+try {
+ const userFound= await User.findOne({email});
+ if (!userFound) return res.status(400).json({menssage: "User not found"});
+const isMatch= bcrypt.compare(password, userFound.password);
+if(!isMatch) return res.status(400).json({menssage: "Incorrect password"});
+
+const token= await createAccessToken({ id:userFound._id});
+
+res.cookie('token',token);
+res.json({
+   id:userFound._id,
+   username:userFound.username,
+   email:userFound.email,
+   createdAt:userFound.createdAt,
+   updatedAt:userFound.updatedAt
+ }); 
+} catch (error) {
+res.status(500).json({ menssage: error.menssage});
+}}
+export const logout = (req, res)=>{
+res.cookie('token','',{
+  expires:new Date(0)
+})
+return res.sendStatus(200);
+}
+export const profile=async (req, res)=>{
+ const userFound= await User.findById (req.user.id)
+ if(!userFound)return res.status(400).json({menssage:"User not found"});
+ return res.json({
+  id:userFound._id,
+  username:userFound.username,
+  email: userFound.email,
+  createdAt: userFound.createdAt,
+  updatedAt: userFound.updatedAt,
+
+ }) 
+ console.log(req.user);
+  res.send('profile')
+}
