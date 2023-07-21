bed9deb81aa411c43963f015bb4f660eda87d102
alejandro0 <alejandro0giraldo@hotmail.com>
2023-07-21 16:47:02 -0500

    Descripción del cambio

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,5 +1,16 @@
 import {Router} from 'express'
 import { authRequired } from '../middlewares/validateToken.js';
+import {
+    getTasks,
+    getTask,
+    createTasks,
+    updateTasks,
+    deleteTasks
+} from '../controllers/tasks.controller.js'
 const router=Router();
-router.get('/tasks',authRequired,(req,res)=>res.send('tasks'))
+router.get('/tasks',authRequired,getTasks)
+router.get('/tasks/:id',authRequired,getTask)
+router.post('/tasks',authRequired,createTasks)
+router.delete('/tasks/:id',authRequired,deleteTasks)
+router.put('/tasks/:id',authRequired,updateTasks)
 export default router;
