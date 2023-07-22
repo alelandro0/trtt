23733387f9bb60d1badecfc103a14db5617b1333
alejandro0 <alejandro0giraldo@hotmail.com>
2023-07-22 10:31:39 -0500

    nuevos moldulos

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -6,7 +6,7 @@ export const getTasks=async(req,res)=>{
    res.json(tasks)
 }
 export const createTasks=async(req,res)=>{
-    const {title,description,date}=req.body
+    const {title, description, date}=req.body
     console.log(req.user);
   const newTask=  new Task({
         title,
@@ -14,23 +14,24 @@ export const createTasks=async(req,res)=>{
         date,
         user: req.user.id
     });
-   const saveTask= await newTask.save();
-   res.json(saveTask);
+   const savedTask= await newTask.save();
+   res.json(savedTask);
 };
 export const getTask=async(req,res)=>{
-    const task= await Task.findById(req.params.id)
+    const task= await Task.findById(req.params.id).populate('user')
     if(!task) return res.status(404).json({message:'task not found'})
     res.json(task)
 }
 export const deleteTasks=async(req,res)=>{
     const task= await Task.findByIdAndDelete(req.params.id)
     if(!task) return res.status(404).json({message:'task not found'});
-    return res.sendStatus(201)
+    return res.sendStatus(204)
 }
 export const updateTasks=async(req,res)=>{
-    const task= await Task.findByIdAndUpdate(req.params.id,req.body,{
+    const task= await Task.findByIdAndUpdate(req.params.id, req.body,{
         new:true,
-    })
+    })  
     if(!task) return res.status(404).json({message:'task not found'})
     res.json(task)
 }
+ 
---
+++ src/schemas/auth.schema.js
@@ -0,0 +1,36 @@
+import { z } from "zod";
+export const registerSchema = z.object({
+  username: z.string({
+    required_error: "username is required",
+  }),
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "Email es invalido",
+    }),
+  password: z
+    .string({
+      required_error: "password is required",
+    })
+    .min(6, {
+      message: "password must be at least 6 characters",
+    }),
+});
+export const loginSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email({
+      message: "invalid email",
+    }),
+  password: z
+    .string({
+      required_error: "password is required",
+    })
+    .min(6, {
+      message: "password must be at least 6 characters",
+    }),
+});
